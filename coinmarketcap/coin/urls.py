b334046da1cdf20f36a75039332322a20aa615fb
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-12-17 21:41:24 +0330

    created new routes

--- coinmarketcap/coin/urls.py
+++ coinmarketcap/coin/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views as v
 urlpatterns = [
-    path('data/' , v.ScrapeCoinmarkepcapView),
+    
+    path('data/' , v.ScrapeCoinmarkepcapView , name="createdata"),
     path('json-data/' , v.JsonDataCoinmarkepcapView),
     path('coin-data/<str:symbol>' , v.GetCoinData),
+    path('download-logo/<str:symbol>' , v.save_image_from_url),
     
     
 ]
