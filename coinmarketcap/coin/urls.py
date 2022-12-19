707dd6a4a3c7d57ca0dae8eab1d5fbf0c53acaf8
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-12-19 20:22:10 +0330

    Changes: naming conventions

--- coinmarketcap/coin/urls.py
+++ coinmarketcap/coin/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views as v
 urlpatterns = [
     
-    path('data/' , v.ScrapeCoinmarkepcapView , name="createdata"),
-    path('json-data/' , v.JsonDataCoinmarkepcapView),
-    path('coin-data/<str:symbol>' , v.GetCoinData),
-    path('download-logo/<str:symbol>' , v.download_logo_symbol,name="dowlogo"),
-    path('get-logo/<str:symbol>' , v.get_logo_symbol , name="getlogo"),
+    path('scrape-coins/' , v.scrape_coins_view , name="scrape_coins"),
+    path('get-coins/' , v.get_coins_view , name="get_coins"),
+    path('get-coin/<str:symbol>' , v.get_single_coin_view , name="get_single_coin"),
+    path('download-logo/<str:symbol>' , v.download_logo_symbol_view,name="dow_logo"),
+    path('get-logo/<str:symbol>' , v.get_logo_symbol_view , name="get_logo"),
     
     
 ]
