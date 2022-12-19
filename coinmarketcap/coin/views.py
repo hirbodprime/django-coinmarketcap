c63706962a8819a3ffcb912247a4e2b088ead2f9
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-12-19 20:21:19 +0330

    Changes: naming conventions and added exceptions for all views

--- coinmarketcap/coin/views.py
+++ coinmarketcap/coin/views.py
@@ -1,63 +1,77 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse , HttpRequest ,JsonResponse
-from coinmarketcapscraper.coinsmarketcap import Scraper
-from .models import CoinDataModel 
-import os
-import tempfile
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
+from django.contrib import messages
+from coinmarketcapscraper.coinsmarketcap import Scraper
+from .models import CoinDataModel
+import tempfile
+import os
 
 
-def get_logo_symbol(request, symbol):
+def get_logo_symbol_view(request, symbol):
     try:
         coin = CoinDataModel.objects.get(symbol=symbol)
         if coin.image: 
             return render(request,"singlelogo.html" , {"coin":coin})
         else:
-            return redirect("dowlogo" , symbol=coin.symbol)
+            return redirect("dow_logo" , symbol=coin.symbol)
     except CoinDataModel.DoesNotExist:
-        return HttpResponse("send a bad symbol")
+        messages.add_message(request, messages.SUCCESS, 'please use one of these symbols')
+        return redirect("scrape_coins")
 
-def download_logo_symbol(req,symbol):
-    if CoinDataModel.objects.filter(symbol=symbol):
-        get_coin = Scraper(download_all_logos=False , coin_data=True,coin_data_file=False,download_logo_sybmol=symbol)
-        src_symbol = get_coin.download_logo_symbol()
-        coin = CoinDataModel.objects.get(symbol=symbol)
-        logo_url = src_symbol[0]
-        logo_symbol = src_symbol[1]
-        image_content = src_symbol[2]
-        img_temp = NamedTemporaryFile()
-        img_temp.write(image_content.content)
-        img_temp.flush()
-        if not coin.image:
-            coin.image.save(f"logo-{symbol}.jpg",File(img_temp), save=True)
-        return redirect("getlogo",symbol=logo_symbol)
-    else:
-        return HttpResponse("send a bad symbol")
+def download_logo_symbol_view(req,symbol):
+    try:
+            coin = CoinDataModel.objects.get(symbol=symbol)
+            get_coin = Scraper(download_all_logos=False , coin_data=True,coin_data_file=False,download_logo_sybmol=symbol)
+            src_symbol = get_coin.download_logo_symbol()
+            logo_url = src_symbol[0]
+            logo_symbol = src_symbol[1]
+            image_content = src_symbol[2]
+            img_temp = NamedTemporaryFile()
+            img_temp.write(image_content.content)
+            img_temp.flush()
+            if not coin.image:
+                coin.image.save(f"logo-{symbol}.jpg",File(img_temp), save=True)
+            return redirect("get_logo",symbol=logo_symbol)
+    except:
+        messages.add_message(req, messages.SUCCESS, 'please use one of these symbols')
+        return redirect("scrape_coins")
+def scrape_coins_view(req):
+    try:
+        if CoinDataModel.objects.all():
+            raise Exception("coins already exists!")
+        get_coin = Scraper(download_all_logos=False , coin_data=True,coin_data_file=False)
+        coin_data = get_coin.get_coin_data()
+        for c in range(len(coin_data)):
+            name = coin_data[c]['name']
+            symbol = coin_data[c]['symbol']
+            price = coin_data[c]['price']
+            CoinDataModel.objects.create(name=name , symbol=symbol,price=price)
+        return redirect("get_coins")
+    except:
+        messages.add_message(req, messages.SUCCESS, "coins are already created!")
+        return redirect("get_coins")
 
-def ScrapeCoinmarkepcapView(req):
-    get_coin = Scraper(download_all_logos=False , coin_data=True,coin_data_file=False)
-    coin_data = get_coin.get_coin_data()
-
-    for c in range(len(coin_data)):
-        name = coin_data[c]['name']
-        symbol = coin_data[c]['symbol']
-        price = coin_data[c]['price']
-        CoinDataModel.objects.create(name=name , symbol=symbol,price=price)
-    return HttpResponse("coin data Created!")
-
-def JsonDataCoinmarkepcapView(request):
+def get_coins_view(request):
     content_type = request.META.get('HTTP_ACCEPT', request.META.get('CONTENT_TYPE', 'application/your_default'))
     if content_type == "application/json":
-    # else:    
-        data = list(CoinDataModel.objects.values())
-        return JsonResponse(data,safe = False)
-    # if content_type == "application/xhtml+xml":
+        coins = list(CoinDataModel.objects.all().values())
+        return JsonResponse(coins,safe = False)
     else:    
-        data = CoinDataModel.objects.all()
-        return render(request,"coindata.html" , {"coin":data})
+        coins = CoinDataModel.objects.all()
+        return render(request,"coindata.html" , {"coins":coins})
 
 
-def GetCoinData(req,symbol):
-    model = list(CoinDataModel.objects.filter(symbol=symbol).values())
-    return JsonResponse(model,safe = False)
+def get_single_coin_view(request,symbol):
+    content_type = request.META.get('HTTP_ACCEPT', request.META.get('CONTENT_TYPE', 'application/your_default'))
+    try:
+        html_coin = CoinDataModel.objects.get(symbol=symbol)
+        if content_type == "application/json":
+            json_coin = list(CoinDataModel.objects.filter(symbol=symbol).values())
+            return JsonResponse(json_coin[0],safe = False)
+        else:
+            return render(request, "coindata.html", {"coin": html_coin})
+    except CoinDataModel.DoesNotExist:
+        messages.add_message(request, messages.SUCCESS, "please use one of these symbols")
+        return redirect("scrape_coins")
