e6caa9948a0ce6808c2310efd66c84d2d28bb2d6
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-12-19 20:21:51 +0330

    write tests

--- coinmarketcap/coin/tests.py
+++ coinmarketcap/coin/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase , Client
+from .models import CoinDataModel
+from django.urls import reverse 
+import json
 
-# Create your tests here.
+class TestModels(TestCase):
+    def setUp(self):
+        client = Client()
+        self.get_coins = reverse('get_coins')
+        self.get_single_coin = reverse('get_single_coin',args=["HPF"])
+        self.get_logo = reverse('dow_logo', args=['BTC'])
+        self.coins = CoinDataModel.objects.create(
+            symbol="HPF",
+            name="hashtag poor festival",
+            price="$1.923.312.412"
+        )
+    def test_coins(self):
+        self.assertEquals(self.coins.symbol, "HPF")
+        self.assertEquals(self.coins.name, "hashtag poor festival")
+        self.assertEquals(self.coins.price, "$1.923.312.412")
+
+    def test_logo_symbol(self):
+        res = self.client.get(self.get_coins)
+        self.assertEquals(res.status_code , 200)
+        self.assertTemplateUsed(res,"coindata.html")
+        
+    def test_download_logo(self):
+        res = self.client.get(self.get_logo)
+        self.assertEquals(res.status_code, 302)
+
+    def test_get_single_coin(self):
+        res = self.client.get(self.get_single_coin)
+        self.assertEquals(res.status_code, 200)
+        self.assertTemplateUsed(res,"coindata.html")
