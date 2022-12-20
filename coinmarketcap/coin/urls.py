9c6a1dfddb5a908267a9bc81340d7beee6929afd
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-12-20 12:51:46 +0330

    modified urls

--- coinmarketcap/coin/urls.py
+++ coinmarketcap/coin/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     
     path('scrape-coins/' , v.scrape_coins_view , name="scrape_coins"),
     path('get-coins/' , v.get_coins_view , name="get_coins"),
-    path('get-coin/<str:symbol>' , v.get_single_coin_view , name="get_single_coin"),
+    path('get-coin/<str:symbol_or_name>' , v.get_single_coin_view , name="get_single_coin"),
     path('download-logo/<str:symbol>' , v.download_logo_symbol_view,name="dow_logo"),
     path('get-logo/<str:symbol>' , v.get_logo_symbol_view , name="get_logo"),
     
