9f81cf93d9ecf687921375769c6a55f1e110c31a
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-12-20 12:52:57 +0330

    added home.urls to route

--- coinmarketcap/coinmarketcap/urls.py
+++ coinmarketcap/coinmarketcap/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 
     path('admin/' , admin.site.urls),
     path('coin/', include("coin.urls")),
+    path('', include("home.urls")),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
