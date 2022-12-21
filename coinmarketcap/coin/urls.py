37dde8e1825c38bc06bb0b7a778d770d5ccc25db
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-12-22 00:50:43 +0330

    added a new route for coin api

--- coinmarketcap/coin/urls.py
+++ coinmarketcap/coin/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views as v
+
 urlpatterns = [
-    
+
+    path('api/get-coins/' , v.CoinsListApiView.as_view() , name="get_coins_api"),
+    path('api/get-coin/<str:name_or_symbol>' , v.CoinsSearchApiView.as_view() , name="get_single_coin_search_api"),
     path('scrape-coins/' , v.scrape_coins_view , name="scrape_coins"),
     path('get-coins/' , v.get_coins_view , name="get_coins"),
     path('get-coin/<str:symbol_or_name>' , v.get_single_coin_view , name="get_single_coin"),
     path('download-logo/<str:symbol>' , v.download_logo_symbol_view,name="dow_logo"),
     path('get-logo/<str:symbol>' , v.get_logo_symbol_view , name="get_logo"),
-    
-    
+
+
 ]
