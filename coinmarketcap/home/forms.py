c297202845da00c1f4572eb5bc0718e7a6189cc1
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-12-22 00:52:19 +0330

    added attrs to input form

--- coinmarketcap/home/forms.py
+++ coinmarketcap/home/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 
 class get_single_coin_form(forms.Form):
-    symbol_or_name = forms.CharField(max_length=30)
+    symbol_or_name = forms.CharField(widget=forms.TextInput(attrs={'id':'input-id','class':'input-class'}))
     def __init__(self, *args, **kwargs):
         super(get_single_coin_form, self).__init__(*args, **kwargs)
         self.fields['symbol_or_name'].label = ""
+        # for visible in self.visible_fields():
+        #     visible.field.widget.attrs['class'] = 'input-class'
