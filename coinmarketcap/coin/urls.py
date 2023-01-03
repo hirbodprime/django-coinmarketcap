49a7c901009fac6ed58315d333959adef312cedd
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2023-01-03 11:00:53 +0330

    coin

--- coinmarketcap/coin/urls.py
+++ coinmarketcap/coin/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('scrape-coins/' , v.scrape_coins_view , name="scrape_coins"),
     path('get-coins/' , v.get_coins_view , name="get_coins"),
     path('get-coin/<str:symbol_or_name>' , v.get_single_coin_view , name="get_single_coin"),
-    path('download-logo/<str:symbol>' , v.download_logo_symbol_view,name="dow_logo"),
-    path('get-logo/<str:symbol>' , v.get_logo_symbol_view , name="get_logo"),
+    path('get-logo/<str:symbol>' , v.download_logo_symbol_view,name="dow_logo"),
 
 
 ]
--- coinmarketcap/coin/views.py
+++ coinmarketcap/coin/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render , redirect
-from django.http import HttpResponse , HttpResponseNotFound ,JsonResponse
+from django.http import HttpResponse , HttpResponseNotFound ,JsonResponse,HttpResponseRedirect
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
 from django.contrib import messages
+from django.urls import reverse
 from django.views.generic import ListView
 from django.db.models import Q
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView , RetrieveAPIView
 from coinmarketcapscraper.coinsmarketcap import Scraper
 from .models import CoinDataModel
-from .serializers import CoinsListApiSerializer
+from .serializers import CoinsListApiSerializer 
 from home.forms import get_single_coin_form
 import tempfile
 import os
@@ -24,78 +25,64 @@ class CoinsSearchApiView(ListAPIView):
     )
 
 
+
 class CoinsListApiView(ListAPIView):
     queryset = CoinDataModel.objects.all()
     serializer_class = CoinsListApiSerializer
 
-def get_logo_symbol_view(request, symbol):
-    try:
-        coin = CoinDataModel.objects.get(symbol=symbol)
-        print(coin.symbol)
-        if coin.image:
-            return render(request,"single-logo.html" , {"coin":coin})
-        else:
-            return redirect("dow_logo" , symbol=coin.symbol)
-    except CoinDataModel.DoesNotExist:
-        messages.add_message(request, messages.SUCCESS, 'please use one of these symbols')
-        return redirect("scrape_coins")
-
-
 def download_logo_symbol_view(req,symbol):
-    try:
+    if CoinDataModel.objects.get(symbol=symbol):
         coin = CoinDataModel.objects.get(symbol=symbol)
-        get_coin = Scraper(download_all_logos=False , coin_data=True,coin_data_file=False,download_logo_sybmol=symbol)
-        src_symbol = get_coin.download_logo_symbol()
-        logo_url = src_symbol[0]
-        logo_symbol = src_symbol[1]
-        image_content = src_symbol[2]
-        img_temp = NamedTemporaryFile()
-        img_temp.write(image_content.content)
-        img_temp.flush()    
         if not coin.image:
+            get_coin = Scraper(download_all_logos=False , coin_data=True,coin_data_file=False,download_logo_sybmol=symbol)
+            src_symbol = get_coin.download_logo_symbol()
+            logo_url = src_symbol[0]
+            logo_symbol = src_symbol[1]
+            image_content = src_symbol[2]
+            img_temp = NamedTemporaryFile()
+            img_temp.write(image_content.content)
+            img_temp.flush()    
             coin.image.save(f"logo-{symbol}.jpg",File(img_temp), save=True)
-        return redirect("get_logo",symbol=logo_symbol)
-    except:
-        messages.add_message(req, messages.SUCCESS, 'please use one of these symbols')
-        return redirect("get_coins")
+            img_temp.close()
+            if CoinDataModel.objects.get(symbol=logo_symbol):
+                coin = CoinDataModel.objects.get(symbol=logo_symbol)
+                return render(req,"single-logo.html" , {"coin":coin})
+                # return redirect("get_logo",symbol=logo_symbol)
+            else:
+                messages.add_message(req, messages.SUCCESS, 'please use one of these symbols')
+                return redirect("get_coins")
+        else:
+            return render(req,"single-logo.html" , {"coin":coin})
+    else:
+        pass
 
 
 def scrape_coins_view(req):
-    try:
-        if CoinDataModel.objects.all():
-            raise Exception("coins already exists!")
-        get_coin = Scraper(download_all_logos=False , coin_data=True,coin_data_file=False)
-        coin_data = get_coin.get_coin_data()
-        for c in range(len(coin_data)):
-            name = coin_data[c]['name']
-            symbol = coin_data[c]['symbol']
-            price = coin_data[c]['price']
-            CoinDataModel.objects.create(name=name , symbol=symbol,price=price)
-        messages.add_message(req, messages.SUCCESS, 'coins created!')
-        return redirect("get_coins")
-    except:
-        messages.add_message(req, messages.SUCCESS, "coins are already created!")
-        return redirect("get_coins")
+    get_coin = Scraper(download_all_logos=False , coin_data=True,coin_data_file=False)
+    coin_data = get_coin.get_coin_data()
+    for c in range(len(coin_data)):
+        name = coin_data[c]['name']
+        symbol = coin_data[c]['symbol']
+        price = coin_data[c]['price']
+        CoinDataModel.objects.update_or_create(name=name , symbol=symbol,
+        defaults={'price':price})
+    messages.add_message(req, messages.SUCCESS, 'coins scraped!')
+    return redirect("get_coins")
 
 
 def get_coins_view(request):
-    form = get_single_coin_form(request.POST or None)
-    if request.method == "GET":
+    if CoinDataModel.objects.all():
         content_type = request.META.get('HTTP_ACCEPT', request.META.get('CONTENT_TYPE', 'application/your_default'))
         if content_type == "application/json":
             coins = list(CoinDataModel.objects.all().values())
             return JsonResponse(coins,safe = False)
-        else:    
+        else:
             coins = CoinDataModel.objects.all()
-            return render(request,"coins.html" , {"coins":coins,'form':form})
-    elif request.method == "POST":
-        if form.is_valid():
-            symbol_or_name = form.cleaned_data['symbol_or_name']
-            coins = CoinDataModel.objects.filter(
-                Q(name__startswith=symbol_or_name) |
-                Q(symbol__startswith=symbol_or_name)
-            )
-            return render(request,"coins.html" , {"coins":coins,'form':form})
+            return render(request,"coins.html" , {"coins":coins})
+    else:
+        CoinDataModel.objects.all().delete()
+        return redirect("scrape_coins")
+
 
 
 
@@ -108,6 +95,7 @@ def get_single_coin_view(request,symbol_or_name):
             return JsonResponse(json_coin[0],safe = False)
         else:
             return render(request, "coin-detail.html", {"coin": html_coin})
+        
     except CoinDataModel.DoesNotExist:
         try:
             html_coin = CoinDataModel.objects.get(name=symbol_or_name)
